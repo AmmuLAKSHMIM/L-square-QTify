cd3e071775dcb3c404984e29e5a37ce18c33fd7f
AmmuLAKSHMIM <ammulaksh2256@gmail.com>
2026-06-13 18:55:26 +0530

    ME_BUILDOUT_QTIFY_V2_MODULE_ONE

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
-// Double check if your folder is "assets" (lowercase) or "Assets" (uppercase)
-import LogoImg from "./Assets/Logo1.png"
+ import LogoImg from "./Assets/Logo1.png"
 // import styles from "../CSS/Logo.module.css"
 
 
 const Logo = () => {
   return (
     <>
+    <div style={{ display: "flex", alignItems: "center"}}>
        <img 
       src={LogoImg} 
       alt="QTify Logo" 
       width={67} 
       style={{ display: "block" }} 
     />
+    </div>
     </>
   )
 }
