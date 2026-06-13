1e43c0e93aee0f28f5785c3ac0336f71ce241156
AmmuLAKSHMIM <ammulaksh2256@gmail.com>
2026-06-13 19:28:47 +0530

    ME_BUILDOUT_QTIFY_V2_MODULE_ONE

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// 🔹 FIX: Mock Swiper components so Jest ignores the module resolution error
+jest.mock('swiper/react', () => ({
+  Swiper: ({ children }) => <div className="mock-swiper">{children}</div>,
+  SwiperSlide: ({ children }) => <div className="mock-swiper-slide">{children}</div>,
+}));
+
+jest.mock('swiper/modules', () => ({
+  Navigation: () => null,
+}));
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
